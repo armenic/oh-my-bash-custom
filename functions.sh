1ffc290d7d10fa1f0193d97f95abb48f14a4e1e3
manukyae <edgar.manukyan@roche.com>
2021-06-05 03:29:35 +0000

    add pathlocal

--- functions.sh
+++ functions.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
 groupstr() {
   local user1="$1"
   [[ -z "$user1" ]] && user1="$USER"
   groups "$user1" | tr " " "\n" | grep "[^:]" | grep -v "$USER" | sort
+}
+
+cdfi() {
+  cd /opt/BAA/FI/clinical_studies || exit
+}
+
+pathlocal() {
+  export PATH=${PATH}:.
 }
