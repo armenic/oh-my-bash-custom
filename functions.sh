d83e9474f598a97c82827613c56f8ebd7af3ae28
manukyae <edgar.manukyan@roche.com>
2021-06-06 17:11:43 +0000

    add comments

--- functions.sh
+++ functions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# modify the output of `groups` command into multiline and sort it
+# designed to work with one user name or use current user if one not supplied
 groupstr() {
   local user1="$1"
   [[ -z "$user1" ]] && user1="$USER"
+  # replace space with new line, rid off line with :, rid of line with user name 
   groups "$user1" | tr " " "\n" | grep "[^:]" | grep -v "$USER" | sort
 }
 
@@ -10,6 +13,7 @@ cdfi() {
   cd /opt/BAA/FI/clinical_studies || exit
 }
 
+# make current dir searchable
 pathlocal() {
   export PATH=${PATH}:.
 }
